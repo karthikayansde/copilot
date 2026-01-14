b56c88d6e9f0b84549d18dbfe0e63868b211f143
Madhacky <mdshahjada747@gmail.com>
2026-01-14 18:39:25 +0530

    download implemented7

--- lib/services/api/endpoints.dart
+++ lib/services/api/endpoints.dart
@@ -4,6 +4,7 @@
     static String get insightBaseUrl => 'http://apihub.pilogcloud.com:6670';
     static String get askQuestionBaseUrl => 'http://apihub.pilogcloud.com:6732';
     static String get chatWithDataMobBaseUrl => 'http://apihub.pilogcloud.com:6676';
+    static String get chatWithDataBaseUrl => 'http://apihub.pilogcloud.com:6730';
     static String get login => '/auth/login';
     static String get signUp => '/auth/register';
     static String get ask => '/ask';
@@ -16,4 +17,5 @@
     static String get dataInsights => '/data_insights/';
     static String get askQuestion => '/ask_question';
     static String get chatWithDataMob => '/chat_with_data_mob';
+    static String get chatWithData => '/chat_with_data';
   }
